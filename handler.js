9c127caddbcc04e4101f7391949f02585415f85a
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-05 18:08:47 -0400

    revert
    reverting back hander.js

--- handler.js
+++ handler.js
@@ -1,25 +1,11 @@
 'use strict';
 
-module.exports.authorize = async event => {
+module.exports.qoutes = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully. Test',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
-
-module.exports.cat = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Cat picture!',
         input: event,
       },
       null,
