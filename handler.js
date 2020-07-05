dc91869beb30ec6b9c2508f85d2360a3d33f2692
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-05 18:08:22 -0400

    revert back authorization
    reverting back.

--- handler.js
+++ handler.js
@@ -12,7 +12,18 @@ module.exports.authorize = async event => {
       2
     ),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.cat = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Cat picture!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
 };
