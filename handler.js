b58097675a91d3728d637e6144225c64962bef60
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-05 17:11:35 -0400

    updated function name
    updated the function name to authorize

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.authorize = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v1.0! Your function executed successfully. Test',
         input: event,
       },
       null,
