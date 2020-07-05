ed6532e75569276df580bd1f207da9b8066191fe
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-05 19:14:08 -0400

    Update handler.js
    Added the generated slack oauth token to aws lambda's enviornment.
    Adding reference to that oauth token.
    
    Added a new function to post the message to slack using slack's api.
    Method: https://api.slack.com/methods/chat.postMessage

--- handler.js
+++ handler.js
@@ -2,12 +2,37 @@
 
 let request = require('request');
 
+//const SLACK_OAUTH_TOKEN = 'XXXX'; //local testing
+const SLACK_OAUTH_TOKEN = process.env.SLACK_OAUTH_ACCESS_TOKEN
+
 module.exports.qoutes = (event) => {
-  getQoutes();
+  getQoute();
 };
 
-function getQoutes() {
+function getQoute() {
   request('https://ron-swanson-quotes.herokuapp.com/v2/quotes', function (err, resp, body) {
+    console.log('error:', err)
+    console.log('statusCode:', resp && resp.statusCode)
+    console.log('body', body)
+    postQoute(body.substring(2, body.length - 2))
+  })
+}
+
+function postQoute(quote) {
+  let options = {
+    url: 'https://slack.com/api/chat.postMessage',
+    headers: {
+      'Accept': 'application/json',
+    },
+    method: 'POST',
+    form: {
+      token: SLACK_OAUTH_TOKEN,
+      channel: 'test', //created a test channel
+      text: quote,
+    }
+  }
+
+  request(options, function(err, resp, body) {
     console.log('error:', err)
     console.log('statusCode:', resp && resp.statusCode)
     console.log('body', body)
